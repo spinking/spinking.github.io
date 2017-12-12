ded643283924ed8a88e756aba24e11e03abf742c
Sergey <spinking@mail.ru>
2017-12-12 13:55:49 +0400

    for two players
    two players only

--- xoxox/script.js
+++ xoxox/script.js
@@ -14,35 +14,20 @@ var view = {
 // Модель
 
 var model = {
-	clank: 0,
 	cells: {
 		locations: ["00", "01", "02", "10", "11", "12", "20", "21", "22"],
 		hits: ["", "", "", "", "", "", "", "", ""]
 	},
 
-	/*winCells: {
-		win1: ["hit", "hit", "hit", "" , "", "", "" , "", ""],
-		win2: ["", "", "", "hit" , "hit", "hit", "" , "", ""],
-		win3: ["", "", "", "" , "", "", "hit" , "hit", "hit"],
-		win4: ["hit", "", "", "hit" , "", "", "hit" , "", ""],
-		win5: ["", "hit", "", "" , "hit", "", "" , "hit", ""],
-		win6: ["", "", "hit", "" , "", "hit", "" , "", "hit"],
-		win7: ["hit", "", "", "" , "hit", "", "" , "", "hit"],
-		win8: ["", "", "hit", "" , "hit", "", "hit" , "", ""]
-	},*/
-
 	twoVariant: function() {
 		check = this.cells.hits;
-		for (var i = 0; i < check.length; i++) {
-				if (controller.guesses % 2 == 0) {
-					view.displayMessage("Oooops");
+				if (controller.guesses % 2 === 0) {
+					view.displayMessage("X turn");
 					return "o";
 				} else { 
-					view.displayMessage("Wooops");
+					view.displayMessage("O turn");
 					return "x";
 				}
-		}
-		
 	},
 
 	getHit: function(guess) {
@@ -50,12 +35,48 @@ var model = {
 		var check = this.cells.hits;
 		for (var i =0; i < loc.length; i++ ) {
 			var index = loc.indexOf(guess);
-			if (index >= 0) {
+			if (index >= 0 && check[index] == "") {
 				check[index] = this.twoVariant();
 					view.displayChar(guess);
+					this.collisions();
+					
+					console.log(guess);
+					console.log(this.cells.hits);
 			} 
 		}
 
+	},
+
+	collisions: function() {
+		check = this.cells.hits;
+		if (
+			(check[0] === "x" && check[1] === "x" && check[2] === "x" ) ||
+			(check[3] === "x" && check[4] === "x" && check[5] === "x" ) ||
+			(check[6] === "x" && check[7] === "x" && check[8] === "x" ) ||
+			(check[0] === "x" && check[3] === "x" && check[6] === "x" ) ||
+			(check[1] === "x" && check[4] === "x" && check[7] === "x" ) ||
+			(check[2] === "x" && check[5] === "x" && check[8] === "x" ) ||
+			(check[0] === "x" && check[4] === "x" && check[8] === "x" ) ||
+			(check[2] === "x" && check[4] === "x" && check[6] === "x" )
+			) {
+			view.displayMessage("X win the game!");
+			this.cells.hits = ["g", "g", "g", "g", "g", "g", "g", "g", "g"];
+
+		} else if (
+			(check[0] === "o" && check[1] === "o" && check[2] === "o" ) ||
+			(check[3] === "o" && check[4] === "o" && check[5] === "o" ) ||
+			(check[6] === "o" && check[7] === "o" && check[8] === "o" ) ||
+			(check[0] === "o" && check[3] === "o" && check[6] === "o" ) ||
+			(check[1] === "o" && check[4] === "o" && check[7] === "o" ) ||
+			(check[2] === "o" && check[5] === "o" && check[8] === "o" ) ||
+			(check[0] === "o" && check[4] === "o" && check[8] === "o" ) ||
+			(check[2] === "o" && check[4] === "o" && check[6] === "o" )
+			) {
+			view.displayMessage("O win the game!");
+			this.cells.hits = ["g", "g", "g", "g", "g", "g", "g", "g", "g"];
+		} else if (controller.guesses >= 9) {
+			view.displayMessage("End this Game!");
+		}
 	}
 }
 
@@ -83,8 +104,6 @@ function init() {
 	var guess = clickID.target.id;
 	controller.processGuess(guess);
 	}
-
-
 }
 
 window.onload = init;
